81f1cd9c2538d407e3fc52427efdc53fde74c6db
Ravil <ravil.vasilev@yandex.ru>
2023-05-14 19:21:47 +0300

    Добавил конфиг для sass

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -6,7 +6,19 @@ export function buildLoaders(): webpack.RuleSetRule[] {
         use: 'ts-loader',
         exclude: /node_modules/,
     }
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    }
     return [
         typescriptLoader,
+        cssLoader,
     ]
 }
