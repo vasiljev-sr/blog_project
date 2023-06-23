a9711db2b69211bdb4b3ccae2740732926518c07
Ravil <ravil.vasilev@yandex.ru>
2023-06-23 10:26:27 +0300

    Настроил eslint

--- src/shared/config/theme/ThemeContext/useTheme.tsx
+++ src/shared/config/theme/ThemeContext/useTheme.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import  { useContext } from 'react';
 import { LOCAL_STORAGE_THEME_KEY, ThemeContext } from './ThemeContext';
 
 export const useTheme = () => {
