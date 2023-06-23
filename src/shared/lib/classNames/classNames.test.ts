88eb75b7dfd87b7a98eedea6495d5b36a6e3581d
Ravil <ravil.vasilev@yandex.ru>
2023-06-23 16:14:24 +0300

    Добавил jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,17 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+  test('С основным классом', () => {
+    expect(classNames('class')).toBe('class');
+  });
+  test('С массивом additional', () => {
+    const additional = ['class1', 'class2'];
+    const expected = 'class class1 class2';
+    expect(classNames('class', {}, additional)).toBe(expected);
+  });
+  test('С объектом mods', () => {
+    const mods = { hovered: true, expected: false };
+    const expected = 'class hovered';
+    expect(classNames('class', mods)).toBe(expected);
+  });
+});
