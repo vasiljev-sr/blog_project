2367db18f5240ce6042495bbcc44750608d708fa
Ravil <ravil.vasilev@yandex.ru>
2023-06-27 14:31:11 +0300

    fix eslint bugs

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,20 +1,22 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Navbar.module.scss';
 import { AppLink } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 
 interface NavbarProps {
   className?: string;
 }
 export const Navbar = (props: NavbarProps) => {
   const { className } = props;
+  const { t } = useTranslation();
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
       <div className={cls.links}>
         <AppLink to={'/'} className={cls.mainLink} theme={'secondary'}>
-          Home
+          {t('Главная')}
         </AppLink>
         <AppLink to={'/about'} theme={'secondary'}>
-          About
+          {t('О проеке')}
         </AppLink>
       </div>
     </div>
