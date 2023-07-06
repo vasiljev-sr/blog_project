190c7329009ebbdcc63fda42834d1c397c13b69d
Ravil <ravil.vasilev@yandex.ru>
2023-07-06 13:14:09 +0300

    fix unit tests

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -7,9 +7,8 @@ describe('Button', () => {
     expect(screen.getByText('BUTTON')).toBeInTheDocument();
   });
 
-  test('has "clear" class by default', () => {
-    render(<Button>BUTTON</Button>);
+  test('has "clear" class', () => {
+    render(<Button theme={'clear'}>BUTTON</Button>);
     expect(screen.getByText('BUTTON')).toHaveClass('clear');
-    screen.debug();
   });
 });
